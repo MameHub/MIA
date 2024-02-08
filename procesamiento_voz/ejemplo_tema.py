a3a2463e3073bd383bf8a36eb23cd3db69a030b6
Álvaro Cañas <acaygo@gmail.com>
2024-02-08 19:17:05 +0100

    ejemplo comentado

--- procesamiento_voz/ejemplo_tema.py
+++ procesamiento_voz/ejemplo_tema.py
@@ -1,7 +1,13 @@
+# Importamos la libreria "speech_recognition" y le asignamos el alias "sr", este nos facilitará las funciones necesarias.
 import speech_recognition as sr
+# Instanciamos el reconocimiento de voz.
 recognizer = sr.Recognizer()
+# Almacenamos el audio recogido por el microfono en la variable "mic".
 mic = sr.Microphone()
+# Definimos la variable "mic" como "source" y almacenamos en la variable "audio" lo que se obtenga con la función "listen" de la instancia de "recognizer" de la fuente "mic".
 with mic as source:
     audio = recognizer.listen(source)
-text = recognizer.recognize_google(audio, language = 'ES')
+# Almacenamos en la variable "text" el audio obtenido en texto, en idioma español.
+text = recognizer.recognize_google(audio, language = 'EN')
+# Imprimimos por pantalla el audio pasado a texto.
 print(f'Has dicho: {text}')
