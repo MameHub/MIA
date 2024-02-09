69f5e997ff01230244122dc6a7c2cb9de8d7fe82
Álvaro Cañas <acaygo@gmail.com>
2024-02-09 02:01:16 +0100

    ah

--- procesamiento_voz/ejemplo_2.py
+++ procesamiento_voz/ejemplo_2.py
@@ -3,9 +3,12 @@ MODIFICAR
 """
 
 import speech_recognition as sr
+
 rec = sr.Recognizer()
 mic = sr.Microphone()
+
 with mic as source:
     audio = rec.listen(source)
+
 to_text = rec.recognize_google(audio, language='ES')
 print(to_text)
